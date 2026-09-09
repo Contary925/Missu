c7f4a2e2e9b1d7535ec8869985c2c2233e948bf6
Kissu <Contary295@gmail.com>
2026-09-09 23:09:54 +0300

    Auto-commit on bot shutdown

--- functions/on_msg.py
+++ functions/on_msg.py
@@ -6,13 +6,15 @@ from functions.reply import check_for_replies
 prefix1 = 'uwu'
 prefix2 = 'уву'
 async def on_msg(client, message) :
-    if message.author.bot :
-        return
-    if message.author == client.user:
+    if message.author.id == '1529029241566662746':
         return
     await check_for_reactions(message) #the only thing the bot can do without a prefix - check if the message contains anything
         #it's supposed to react or reply to
     await check_for_replies(message)
+    if message.author.bot :
+        return
+    if message.author == client.user:
+        return
     content = None
     if message.content.lower().startswith(prefix1) :
         content = cutword(message.content, prefix1)
